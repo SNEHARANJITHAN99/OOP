45a7cd6643c7a7c85f46c65cf7e57c7234fd6f5c
SnehaRanjithan <sneharanjithan1999@gmail.com>
2021-08-30 16:19:30 +0530

    Labcycle 3

---
+++ LinkedListRemove.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+  public class LinkedListRemove
+   {
+  public static void main(String[] args) 
+  {
+    
+          LinkedList<String> l_list = new LinkedList<String>();
+   
+          l_list.add("Red");
+          l_list.add("Green");
+          l_list.add("Black");
+          l_list.add("Pink");
+          l_list.add("orange");
+          System.out.println("The Original linked list: " + l_list);
+          l_list.clear();
+
+          System.out.println("The New linked list: " + l_list);
+  }
+}
